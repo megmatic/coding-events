36729c49f86302cfd0ac5b1cc7d8f242654c5803
Meg <meggoodwin57@gmail.com>
2021-03-24 21:23:33 -0500

    started java-springboot-thymeleaf-bootstrap project with the code along from launchcode

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,40 @@
+package org.launchcode.codingevents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createNewEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
+}
