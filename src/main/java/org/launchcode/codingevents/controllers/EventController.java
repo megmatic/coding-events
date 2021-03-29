249f8da1d2e353914031d106fb61e1821915560d
Meg <meggoodwin57@gmail.com>
2021-03-29 13:27:38 -0500

    added validation to controller

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,8 +5,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +32,14 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createNewEvent(@ModelAttribute Event  newEvent) {
+    public String createNewEvent(@ModelAttribute @Valid Event  newEvent,
+                                 Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad Data! ");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:";
     }
